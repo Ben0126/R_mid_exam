bf735c55537b1178bf0ea13f9cd214df94d9913b
Ben Yeh <75458848+Ben0126@users.noreply.github.com>
2022-05-01 03:18:03 +0800

    0501

--- tomato.R
+++ tomato.R
@@ -1,62 +1,106 @@
-#Advanced Analytics 
-#mid exam project
-
 # tomato
 tomato = read.csv("TomatoFirst.csv", header = TRUE, sep = ",")
 head(tomato)
+
+## 資料預處理 ----
 str(tomato) # 查看 tomato 內部結構
 tomato[!complete.cases(tomato),]  # 檢查是否有 NA 的資料
 
-# tomato--迴歸分析----
-#迴歸分析
+# test
+test = tomato[10,]
+test = test[,-2]  #刪除Tomato
+test = test[,-3]  #刪除Source
+test = test[,-1]  #刪除round
 
-summary(tomato) # 查看基本統計量
-library(ggplot2)
-library(GGally)
-ggpairs(tomato)
+#tomato
+tomato = tomato[-10,]  #刪除第10列
+tomato = tomato[,-2]  #刪除Tomato
+tomato = tomato[,-3]  #刪除Source
+tomato = tomato[,-1]  #刪除round
+tomato[!complete.cases(tomato),]  # 檢查是否有 NA 的資料
+str(tomato)
+
+## 建立關聯規則----
+summary(tomato) 
+require(arules)
+
+rule <- apriori(tomato, parameter=list(minlen=2, supp=0.01, conf=0.01))
+inspect(rule)
+
+sort.rule <- sort(rule, by="lift")
+inspect(sort.rule)
+
+## 冗規則判斷與去除
+# 先根據 support 大小排序 rules
+sort.rule <- sort(rule, by="support")
+
+# 'arules' version = 1.4-2 , under R-3.2.5
+subset.matrix <- is.subset(x=sort.rule, y=sort.rule)
+
+# 'arules' version = 1.5-2 , under R-3.4.0
+subset.matrix <- as.matrix(is.subset(x=sort.rule, y=sort.rule))
+
+# 把這個矩陣的下三角去除，只留上三角的資訊
+subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
+
+# 計算每個column中TRUE的個數，若有一個以上的TRUE，代表此column是多餘的
+redundant <- colSums(subset.matrix, na.rm=T) >= 1
+
+# 移除多餘的規則
+sort.rule <- sort.rule[!redundant]
+
+inspect(sort.rule)
+
+require(arulesViz)
+plot(sort.rule)
+
+plot(sort.rule, method="graph")
+plot(sort.rule, method="grouped")
 
 
-# 畫出 XY 散佈圖，依據 Species 上色
-require(ggplot2)
-qplot(x = Petal.Length,
-      y = Petal.Width,
-      data = tomato,
-      color = Species)
 
 
-#ERROR
-# 建立迴歸模型
-tomato.lm <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
-              data = tomato)
-# 查看模型配適結果
-summary(tomato.lm)
+## 決策樹 Decision Tree ----
 
-library(ggfortify)
+require(rpart)
 
-# 畫出模型診斷用的圖
-autoplot(tomato.lm)
-
-# 常態性檢定
-shapiro.test(tomato.lm$residual)
-
-# 殘差獨立性檢定
-library(car)
-require(car)
-durbinWatsonTest(tomato.lm)
-
-# 殘差變異數同質性檢定
-require(car)
-ncvTest(tomato.lm)
-
-# 新觀測值
-new.tomato <- data.frame(Sepal.Width=3.1, Petal.Length=1.6, Petal.Width=0.3)
-new.tomato
-
-# 預測資料
-predict(tomato.lm, new.tomato)
+set.seed(22)
+train.index <- sample(x=1:nrow(tomato), size=ceiling(1*nrow(tomato) ))
+train <- tomato[train.index, ]
 
 
-# tomato--
+# CART的模型：把存活與否的變數(Survived)當作Y，剩下的變數當作X
+cart.model<- rpart(Price~. ,  data=train)
+cart.model
 
-# 主成份分析 Principal Component Analysis ----
+require(rpart.plot) 
+prp(cart.model,         # 模型
+    faclen=0,           # 呈現的變數不要縮寫
+    extra=1)
+
+require(partykit)   
+rparty.tree <- as.party(cart.model) # 轉換cart決策樹
+rparty.tree # 輸出各節點的細部資訊
+
+plot(rparty.tree) 
+plotcp(cart.model) # 畫圖觀察未修剪的樹
+
+prunetree_cart.model <- prune(cart.model, 
+                              cp = cart.model$cptable[which.min(cart.model$cptable[,"xerror"]),
+                                                      "CP"]) # 利用能使決策樹具有最小誤差的CP來修剪樹
+
+prunetree_pred <- predict(cart.model,test)
+prunetree_pred
+
+
+require(caret)
+require(e1071)
+train_control <- trainControl(method="cv", number=10)
+train_control.model <- train(Price~., data=train, method="rpart", trControl=train_control)
+train_control.model
+
+## 預測 ----
+
+pred <- predict(cart.model,test)
+pred
 
--- wine.R
+++ wine.R
@@ -7,48 +7,149 @@ head(wine)
 str(wine) # 查看 wine 內部結構
 wine[!complete.cases(wine),]  # 檢查是否有 NA 的資料
 
-# wine--迴歸分析---------------------------------------------------------
-#迴歸分析
 
-summary(wine) # 查看基本統計量
-library(ggplot2)
-library(GGally)
-ggpairs(wine)
+## 決策樹 Decision Tree ----
+require(rpart)
+
+# 先把資料區分成 train=0.8, test=0.2 
+set.seed(22)
+train.index <- sample(x=1:nrow(wine), size=ceiling(0.8*nrow(wine) ))
+train <- wine[train.index, ]
+test <- wine[-train.index, ]
+
+# CART的模型：酒精濃度(Alcohol)的變數(Survived)當作Y，剩下的變數當作X
+cart.model<- rpart(Alcohol ~. , 
+                   data=train)
+
+# 輸出各節點的細部資訊(呈現在console視窗)
+cart.model
+
+require(rpart.plot) 
+prp(cart.model,         # 模型
+    faclen=0,           # 呈現的變數不要縮寫
+    fallen.leaves=TRUE, # 讓樹枝以垂直方式呈現
+    shadow.col="gray",  # 最下面的節點塗上陰影
+    # number of correct classifications / number of observations in that node
+    extra=1)  
+
+require(partykit)   
+rparty.tree <- as.party(cart.model) # 轉換cart決策樹
+rparty.tree # 輸出各節點的細部資訊
 
 
-# 畫出 XY 散佈圖，依據 Species 上色
-require(ggplot2)
-qplot(x = Petal.Length,
-      y = Petal.Width,
-      data = wine,
-      color = Species)
+plot(rparty.tree) 
 
-# 建立迴歸模型
-wine.lm <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
-              data = wine)
-# 查看模型配適結果
-summary(wine.lm)
+pred <- predict(cart.model, test)
 
-library(ggfortify)
+# 用table看預測的情況
+table(real=test$Alcohol, predict=pred)
 
-# 畫出模型診斷用的圖
-autoplot(wine.lm)
+# 計算預測準確率 = 對角線的數量/總數量
+confus.matrix <- table(real=test$Alcohol, predict=pred)
+sum(diag(confus.matrix))/sum(confus.matrix) # 對角線的數量/總數量
 
-# 常態性檢定
-shapiro.test(wine.lm$residual)
+printcp(cart.model) # 先觀察未修剪的樹，CP欄位代表樹的成本複雜度參數
 
-# 殘差獨立性檢定
-library(car)
-require(car)
-durbinWatsonTest(wine.lm)
+plotcp(cart.model) # 畫圖觀察未修剪的樹
 
-# 殘差變異數同質性檢定
-require(car)
-ncvTest(wine.lm)
+prunetree_cart.model <- prune(cart.model, 
+                              cp = cart.model$cptable[which.min(cart.model$cptable[,"xerror"]),
+                                                      "CP"]) # 利用能使決策樹具有最小誤差的CP來修剪樹
 
-# 新觀測值
-new.wine <- data.frame(Sepal.Width=3.1, Petal.Length=1.6, Petal.Width=0.3)
-new.wine
+prunetree_pred <- predict(prunetree_cart.model,test)
 
-# 預測資料
-predict(wine.lm, new.wine)
+# 用table看預測的情況
+table(real=test$Alcohol, predict=prunetree_pred)
+
+prunetree_confus.matrix <- table(real=test$Alcohol, predict=prunetree_pred)
+sum(diag(prunetree_confus.matrix))/sum(prunetree_confus.matrix) # 對角線的數量/總數量
+
+require(caret)
+require(e1071)
+train_control <- trainControl(method="cv", number=10)
+train_control.model <- train(Alcohol~., data=train, method="rpart", trControl=train_control)
+train_control.model
+
+
+## 類神經網路 ANN----
+require(neuralnet) # for neuralnet(), nn model
+require(nnet)      # for class.ind()
+require(caret)     # for train(), tune parameters
+
+data <- wine
+
+
+
+# 原始資料就會變成像這樣
+head(data)
+
+formula.bpn <- Alcohol ~  Cultivar+ Malic.acid + Ash + Alcalinity.of.ash  + Magnesium + Total.phenols + Flavanoids + Nonflavanoid.phenols + Proanthocyanins + Color.intensity + Hue + OD280.OD315.of.diluted.wines + Proline  
+bpn <- neuralnet(formula = formula.bpn, data = data, hidden = c(2,4,2), learningrate = 0.01)    
+                 
+# bpn模型會長得像這樣
+plot(bpn)
+
+## Tuning Parameters
+# nrow()是用來擷取資料筆數，乘上0.8後，表示我們的train set裡面要有多少筆資料(data size)
+smp.size <- floor(0.8*nrow(data)) 
+
+# 因為是抽樣，有可能每次抽樣結果都不一樣，因此這裡規定好亂數表，讓每次抽樣的結果一樣
+set.seed(131)         
+
+# 從原始資料裡面，抽出train set所需要的資料筆數(data size)
+train.ind <- sample(seq_len(nrow(data)), smp.size)
+
+# 分成train/test
+train <- data[train.ind, ]
+test <- data[-train.ind, ]
+
+# tune parameters
+model <- train(form=formula.bpn,     # formula
+               data=train,           # 資料
+               method="neuralnet",   # 類神經網路(bpn)
+               
+               # 最重要的步驟：觀察不同排列組合(第一層1~4個nodes ; 第二層0~4個nodes)
+               # 看何種排列組合(多少隱藏層、每層多少個node)，會有最小的RMSE
+               tuneGrid = expand.grid(.layer1=c(1:4), .layer2=c(0:6), .layer3=c(0:6)),               
+               
+               # 以下的參數設定，和上面的neuralnet內一樣
+               learningrate = 0.01,  
+               threshold = 0.01
+               )
+
+# 顯示最佳解
+model
+
+##  使用新參數重新訓練
+# 將參數組合及RMSE畫成圖
+plot(model)
+
+bpn <- neuralnet(formula = formula.bpn, 
+                 data = train,
+                 hidden = c(4,5,6),     # 第一隱藏層1個node，第二隱藏層2個nodes
+                 learningrate = 0.01, # learning rate
+                 threshold = 0.01,    # partial derivatives of the error function, a stopping criteria
+                 )
+
+# 新的bpn模型會長得像這樣
+plot(bpn)
+
+pred <- compute(bpn,test)  
+pred$net.result
+
+
+
+# 建立一個新欄位，叫做Alcohol
+pred$Alcohol <- ""
+
+# 把結果轉成data frame的型態
+pred <- as.data.frame(pred)
+pred[i, 1]=={pred[i, "Alcohol"] <- pred[i, 1]}
+pred
+
+# 混淆矩陣 (預測率有96.67%)
+table(real = test$Alcohol, predict = pred$Alcohol)
+table(real=test$Alcohol, predict=pred)
+
+prunetree_confus.matrix <- table(real=test$Alcohol, predict=pred$Alcohol)
+sum(diag(prunetree_confus.matrix))/sum(prunetree_confus.matrix) # 對角線的數量/總數量
